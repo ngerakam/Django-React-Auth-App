a3b02fb06bc8caa9ddd12b3eb0d7440292a2f5cb
ngerakam <ngerakam123@gmail.com>
2023-01-23 22:44:29 +0300

    initial commit

---
+++ backend/core/authentication.py
@@ -0,0 +1,71 @@
+import jwt
+import datetime
+from rest_framework import exceptions
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from rest_framework.response import Response
+
+from .models import User
+from .serializers import UserSerializer
+
+
+class JWTAuthentication(BaseAuthentication):
+
+    def authenticate(self, request):
+        auth = get_authorization_header(request).split()
+
+        if auth and len(auth) == 2:
+            token = auth[1].decode('utf-8')
+
+            id = decode_access_token(token)
+
+            user = User.objects.get(pk=id)
+
+            return (user, None)
+
+        raise exceptions.AuthenticationFailed("Unauthenticated")
+
+
+def create_access_token(id):
+
+    return jwt.encode({
+        'user_id': id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30),
+        'iat': datetime.datetime.utcnow()
+    }, 'access_secret', algorithm='HS256')
+
+
+def decode_access_token(token):
+    try:
+
+        # Algorithms with an S
+
+        payload = jwt.decode(token, 'access_secret', algorithms='HS256')
+
+        return payload['user_id']
+
+    except:
+
+        raise exceptions.AuthenticationFailed("Unauthenticated")
+
+
+def create_refresh_token(id):
+
+    return jwt.encode({
+        'user_id': id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7),
+        'iat': datetime.datetime.utcnow()
+    }, 'refresh_secret', algorithm='HS256')
+
+
+def decode_refresh_token(token):
+    try:
+
+        # Algorithms with an S
+
+        payload = jwt.decode(token, 'refresh_secret', algorithms='HS256')
+
+        return payload['user_id']
+
+    except:
+
+        raise exceptions.AuthenticationFailed("Unauthenticated")
